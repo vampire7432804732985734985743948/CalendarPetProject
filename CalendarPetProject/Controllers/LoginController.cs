998f4462ff0d2d03d1c402819da3a9657d821c20
vampire7432804732985734985743948 <romannagik@gmail.com>
2025-06-21 13:40:34 -1100

    .

---
+++ CalendarPetProject/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CalendarPetProject.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ CalendarPetProject/ViewModels/AccountEnterance/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace CalendarPetProject.ViewModels.AccountEnterance
+{
+    [Keyless]
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Email is reqired")]
+        [EmailAddress]
+        public string? Login { get; set; }
+
+        [Required(ErrorMessage = "Password is reqired")]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool DoesRememberUser { get; set; }
+    }
+}
