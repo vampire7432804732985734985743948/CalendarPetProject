17b04241836363f9ef056c7202c93bdc8cb8fb01
vampire7432804732985734985743948 <romannagik@gmail.com>
2025-06-29 10:17:27 -1100

    .

--- CalendarPetProject/Controllers/HomeController.cs
+++ CalendarPetProject/Controllers/HomeController.cs
@@ -13,11 +13,6 @@ namespace CalendarPetProject.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-
         public IActionResult Privacy()
         {
             return View();
@@ -27,6 +22,19 @@ namespace CalendarPetProject.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult Index()
+        {
+            if (User.Identity?.IsAuthenticated == true)
+            {
+                // User is logged in, return the logged-in view or redirect to a dashboard
+                return View("LoggedInView");
+            }
+            else
+            {
+                // User is not logged in, return the guest view or redirect to login page
+                return View("GuestView");
+            }
         }
     }
 }
