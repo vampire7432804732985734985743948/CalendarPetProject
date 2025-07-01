7de37a5ba639147cab0445f12c3d531ad86ba403
vampire7432804732985734985743948 <romannagik@gmail.com>
2025-07-01 12:45:03 -1100

    .

--- CalendarPetProject/wwwroot/js/GuestPage.js
+++ CalendarPetProject/wwwroot/js/GuestPage.js
@@ -1,19 +1,20 @@
 ﻿const images = [
+    "url('../Resources/Images/RandomBackgroundImages/Waterfall1.jpg')",
     "url('../Resources/Images/RandomBackgroundImages/DarkSpace1.jpg')",
     "url('../Resources/Images/RandomBackgroundImages/DarkSpace2.jpg')",
+    "url('../Resources/Images/RandomBackgroundImages/DarkSpace3.jpg')",
+    "url('../Resources/Images/RandomBackgroundImages/DarkSpace4.jpg')",
     "url('../Resources/Images/RandomBackgroundImages/Mountain1.jpg')",
     "url('../Resources/Images/RandomBackgroundImages/Mountain2.jpg')",
     "url('../Resources/Images/RandomBackgroundImages/Forest1.jpg')",
-    "url('../Resources/Images/RandomBackgroundImages/Forest2.jpg')"
+    "url('../Resources/Images/RandomBackgroundImages/Forest2.jpg')",
 ];
 
 let index = 0;
 const slider = document.getElementsByClassName("body-background")[0];
-
-slider.style.backgroundImage = "url('../Resources/Images/RandomBackgroundImages/DarkSpace1.jpg')";
 function updateBackground() {
-    index = (index + 1) % images.length;
-    slider.style.backgroundImage = images[index];
+    const randomIndex = Math.floor(Math.random() * images.length);
+    slider.style.backgroundImage = images[randomIndex];
 }
-
+updateBackground();
 setInterval(updateBackground, 2000);
