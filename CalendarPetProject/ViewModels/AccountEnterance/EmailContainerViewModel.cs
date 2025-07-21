4ea1a4cd67dbd219e11d9dcfa99315185eef17c3
vampire7432804732985734985743948 <romannagik@gmail.com>
2025-07-21 07:00:50 -1100

    .

---
+++ CalendarPetProject/ViewModels/AccountEnterance/EmailContainerViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CalendarPetProject.ViewModels.AccountEnterance
+{
+    public class EmailContainerViewModel
+    {
+        [Display(Name = "Work email")]
+        [Required(ErrorMessage = "Email is reqired")]
+        public string Email { get; set; } = string.Empty;
+    }
+}
