b191c69c56e85f4375dcff987083f835a7c55859
vampire7432804732985734985743948 <romannagik@gmail.com>
2025-08-11 06:52:12 -1100

    .

--- CalendarPetProject/wwwroot/js/site.js
+++ CalendarPetProject/wwwroot/js/site.js
@@ -11,4 +11,54 @@
             .then(() => alert("Email address copied to clipboard!"))
             .catch(err => console.error("Copy failed: ", err));
     }
+    function positionToggleButton() {
+        const footer = document.getElementById("main-footer");
+        const button = document.getElementById("footer-toggle-button");
+
+        const footerHeight = footer.offsetHeight;
+        button.style.bottom = `${footerHeight - 20}px`;
+    }
+
+    window.addEventListener("load", positionToggleButton);
+    window.addEventListener("resize", positionToggleButton);
+
+    const togleFooterButton = document.getElementById("footer-toggle-button");
+    let isFooterHidden = false;
+    function toggleFooter(isClosed) {
+        const animationDuration = 500;
+        const footer = document.getElementById("main-footer");
+        if (!isClosed) {
+            shiftElement(footer, "110px", animationDuration);
+            rotateButton(togleFooterButton, "180deg", animationDuration);
+        } else {
+            shiftElement(footer, "0px", animationDuration);
+            rotateButton(togleFooterButton, "0deg", animationDuration);
+        }
+    }
+    function shiftElement(element, yShift, animationDuration) {
+        if (element != null) {
+            element.animate(
+                [
+                    { transform: "translateY(0px)" },
+                    { transform: `translateY(${yShift})` }
+                ],
+                { duration: animationDuration, fill: "forwards", easing: "ease-in-out" }
+            );
+        }
+    }
+    function rotateButton(button, degrees, animationDuration) {
+        if (button != null) {
+            button.animate(
+                [
+                    { transform: "rotate(0deg)" },
+                    { transform: `rotate(${degrees})` }
+                ],
+                { duration: animationDuration, fill: "forwards" }
+            );
+        }
+    }
+    togleFooterButton.addEventListener('click', () => {
+        toggleFooter(isFooterHidden);
+        isFooterHidden = !isFooterHidden;
+    });
 });
